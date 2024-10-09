c90dc15cd44cae8d35e3128edeb3297c4869dc52
amunger <aamunger@microsoft.com>
2024-10-09 14:55:09 -0700

    exec process to validate/write svg

--- src/diagram.ts
+++ src/diagram.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import { logMessage } from './extension';
+import { exec } from 'child_process';
 
 export class Diagram {
     constructor(private readonly _content: string) {
@@ -9,21 +10,33 @@ export class Diagram {
 
     get content(): string { return this._content; }
 
-    async validate(): Promise<{ message: string, stack: string } | undefined> {
+    async validate(): Promise<{ message: string, stack: string | undefined } | undefined> {
         const tmpDir = fs.mkdtempSync(os.tmpdir());
         logMessage(tmpDir);
 
         // Write the diagram to a file
         fs.writeFileSync(path.join(tmpDir, 'diagram.md'), this.content);
-        const mermaidCLIModule = await import('@mermaid-js/mermaid-cli');
+
         try {
-            await mermaidCLIModule.run(
-                `${tmpDir}/diagram.md`,     // input
-                `${tmpDir}/diagram.svg`,    // output
-                {
-                    outputFormat: 'svg',
-                }
-            );
+
+            return new Promise((resolve, reject) => {
+                exec(`npx mmdc -i ${tmpDir}/diagram.md -o ${tmpDir}/diagram.svg`, (error, stdout, stderr) => {
+                    if (error) {
+                        logMessage(`ERR: ${error.message}`);
+                        resolve({
+                            message: error.message,
+                            stack: error.stack
+                        });
+                    } else if (stderr) {
+                        logMessage(`STDERR: ${stderr}`);
+                        // probably still worked?
+                        resolve(undefined);
+                    } else {
+                        logMessage(`STDOUT: ${stdout}`);
+                        resolve(undefined);
+                    }
+                });
+            });
 
         } catch (e: any) {
             logMessage(`ERR: ${e?.message ?? e}`);
