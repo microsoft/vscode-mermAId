5e369f4968cf697b0c271779ac33a804d9afc67d
Rob Lourens <roblourens@gmail.com>
2024-11-25 11:29:47 +0000

    Adopt tsx PromptElement

--- src/chat/chatParticipant.ts
+++ src/chat/chatParticipant.ts
@@ -1,17 +1,15 @@
 import { sendChatParticipantRequest } from '@vscode/chat-extension-utils';
+import { PromptElementAndProps } from '@vscode/chat-extension-utils/dist/toolsPrompt';
 import * as vscode from 'vscode';
 import { COMMAND_OPEN_DIAGRAM_SVG } from '../commands';
 import { Diagram } from '../diagram';
 import { DiagramEditorPanel } from '../diagramEditorPanel';
 import { logMessage } from '../extension';
-import { makePrompt } from './mermaidPrompt';
-
-let developmentMode = false;
+import { MermaidPrompt } from './mermaidPrompt';
 
 export function registerChatParticipant(context: vscode.ExtensionContext) {
-    const handler: vscode.ChatRequestHandler = chatRequestHandler;
-
-    developmentMode = context.extensionMode === vscode.ExtensionMode.Development;
+    const mermaid = new MermaidChatParticipant(context);
+    const handler: vscode.ChatRequestHandler = mermaid.chatRequestHandler.bind(mermaid);
 
     const participant = vscode.chat.createChatParticipant('copilot-diagram.mermAId', handler);
     participant.iconPath = new vscode.ThemeIcon('pie-chart');
@@ -19,102 +17,113 @@ export function registerChatParticipant(context: vscode.ExtensionContext) {
     DiagramEditorPanel.extensionUri = context.extensionUri;
 }
 
-async function chatRequestHandler(request: vscode.ChatRequest, chatContext: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken) {
-    if (request.command === 'help') {
-        handleHelpCommand(stream);
-        return;
-    } else if (request.command === 'iterate') {
-        const diagram = DiagramEditorPanel.currentPanel?.diagram;
-        if (!diagram) {
-            stream.markdown('No diagram found in editor view. Please create a diagram first to iterate on it.');
+class MermaidChatParticipant {
+    constructor(
+        private readonly extensionContext: vscode.ExtensionContext,
+    ) { }
+
+    async chatRequestHandler(request: vscode.ChatRequest, chatContext: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken) {
+        if (request.command === 'help') {
+            this.handleHelpCommand(stream);
             return;
+        } else if (request.command === 'iterate') {
+            const diagram = DiagramEditorPanel.currentPanel?.diagram;
+            if (!diagram) {
+                stream.markdown('No diagram found in editor view. Please create a diagram first to iterate on it.');
+                return;
+            }
         }
+
+        let retries = 0;
+        let validationError = '';
+        const runRequest = async () => {
+            // I don't know how to configure the types so that this can be inlined
+            const prompt: PromptElementAndProps<MermaidPrompt> = {
+                promptElement: MermaidPrompt,
+                props: { validationError, request }
+            };
+            const result = sendChatParticipantRequest(
+                request,
+                chatContext,
+                {
+                    prompt,
+                    tools: vscode.lm.tools.filter(tool => tool.tags.includes('mermaid')),
+                    responseStreamOptions: {
+                        stream,
+                        references: true,
+                        responseText: false
+                    },
+                    requestJustification: 'To collaborate on diagrams',
+                    extensionMode: this.extensionContext.extensionMode
+                },
+                token);
+
+            let isMermaidDiagramStreamingIn = false;
+            let mermaidDiagram = '';
+
+            let responseStr = '';
+            for await (const part of result.stream) {
+                if (part instanceof vscode.LanguageModelTextPart) {
+                    if (!isMermaidDiagramStreamingIn && part.value.includes('```')) {
+                        // When we see a code block, assume it's a mermaid diagram
+                        stream.progress('Capturing mermaid diagram from the model...');
+                        isMermaidDiagramStreamingIn = true;
+                    }
+
+                    // TODO get multiple diagrams? need to handle the end?
+                    if (isMermaidDiagramStreamingIn) {
+                        // Gather the mermaid diagram so we can validate it
+                        mermaidDiagram += part.value;
+                    } else {
+                        // Otherwise, render the markdown normally
+                        stream.markdown(part.value);
+                        responseStr += part.value;
+                    }
+                }
+            }
+
+            logMessage(mermaidDiagram);
+
+            // Validate
+            stream.progress('Validating mermaid diagram');
+            const diagram = new Diagram(mermaidDiagram);
+            const diagramResult = await DiagramEditorPanel.createOrShow(diagram);
+
+            if (diagramResult.success) {
+                const openMermaidDiagramCommand: vscode.Command = {
+                    command: COMMAND_OPEN_DIAGRAM_SVG,
+                    title: vscode.l10n.t('Open mermaid diagram'),
+                    arguments: [diagram.content]
+                };
+                stream.button(openMermaidDiagramCommand);
+                return await result.result;
+            }
+
+            // -- Handle parse error
+            logMessage(`Not successful (on retry=${++retries})`);
+            if (retries < 3) {
+                if (retries === 1 && mermaidDiagram.indexOf('classDiagram') !== -1) {
+                    stream.progress('Attempting to fix validation errors');
+                    validationError = this.getValidationErrorMessage(diagramResult.error, mermaidDiagram, true);
+                } else {
+                    stream.progress('Attempting to fix validation errors');
+                    validationError = this.getValidationErrorMessage(diagramResult.error, mermaidDiagram, false);
+                }
+                return runRequest();
+            } else {
+                if (diagramResult.error) {
+                    logMessage(diagramResult.error);
+                }
+                stream.markdown('Failed to display your requested mermaid diagram. Check output log for details.\n\n');
+                return await result.result;
+            }
+        };
+
+        return await runRequest();
     }
 
-    let retries = 0;
-    let validationError = '';
-    const runRequest = async () => {
-        const result = sendChatParticipantRequest(
-            request,
-            chatContext,
-            {
-                prompt: makePrompt(request.command, validationError),
-                tools: vscode.lm.tools.filter(tool => tool.tags.includes('mermaid')),
-                responseStreamOptions: {
-                    stream,
-                    references: true,
-                    responseText: false
-                },
-                requestJustification: 'To collaborate on diagrams',
-            },
-            token);
-    
-        let isMermaidDiagramStreamingIn = false;
-        let mermaidDiagram = '';
-    
-        let responseStr = '';
-        for await (const part of result.stream) {
-            if (part instanceof vscode.LanguageModelTextPart) {
-                if (!isMermaidDiagramStreamingIn && part.value.includes('```')) {
-                    // When we see a code block, assume it's a mermaid diagram
-                    stream.progress('Capturing mermaid diagram from the model...');
-                    isMermaidDiagramStreamingIn = true;
-                }
-    
-                // TODO get multiple diagrams? need to handle the end?
-                if (isMermaidDiagramStreamingIn) {
-                    // Gather the mermaid diagram so we can validate it
-                    mermaidDiagram += part.value;
-                } else {
-                    // Otherwise, render the markdown normally
-                    stream.markdown(part.value);
-                    responseStr += part.value;
-                }
-            }
-        }
-    
-        logMessage(mermaidDiagram);
-    
-        // Validate
-        stream.progress('Validating mermaid diagram');
-        const diagram = new Diagram(mermaidDiagram);
-        const diagramResult = await DiagramEditorPanel.createOrShow(diagram);
-    
-        if (diagramResult.success) {
-            const openMermaidDiagramCommand: vscode.Command = {
-                command: COMMAND_OPEN_DIAGRAM_SVG,
-                title: vscode.l10n.t('Open mermaid diagram'),
-                arguments: [diagram.content]
-            };
-            stream.button(openMermaidDiagramCommand);
-            return;
-        }
-    
-        // -- Handle parse error
-        logMessage(`Not successful (on retry=${++retries})`);
-        if (retries < 3) {
-            if (retries === 1 && mermaidDiagram.indexOf('classDiagram') !== -1) {
-                stream.progress('Attempting to fix validation errors');
-                validationError = getValidationErrorMessage(diagramResult.error, mermaidDiagram, true);
-            } else {
-                stream.progress('Attempting to fix validation errors');
-                validationError = getValidationErrorMessage(diagramResult.error, mermaidDiagram, false);
-            }
-            return runRequest();
-        } else {
-            if (diagramResult.error) {
-                logMessage(diagramResult.error);
-            }
-            stream.markdown('Failed to display your requested mermaid diagram. Check output log for details.\n\n');
-            return;
-        }
-    };
-
-    await runRequest();
-}
-
-function handleHelpCommand(stream: vscode.ChatResponseStream) {
-    stream.markdown(`
+    private handleHelpCommand(stream: vscode.ChatResponseStream) {
+        stream.markdown(`
 ## Welcome to the Mermaid Diagram Generator!
 
 Mermaid is a diagramming and charting tool that extends markdown. Visit their [website](https://mermaid.js.org/) to learn more about the tool.
@@ -127,14 +136,15 @@ This chat agent generates useful diagrams using Mermaid to help you better under
 - **/iterate**: To be called when you already have a diagram up to refine, add, and change the existing diagram.
 
 Good luck and happy diagramming!`);
-}
-
-function getValidationErrorMessage(error: string, diagram: string, uml: boolean) {
-    let message = `Please fix this mermaid parse error to make the diagram render correctly: ${error}.\n Here is the diagram you provided:\n${diagram}`;
-    if (uml) {
-        message += fixUmlMessage;
     }
-    return message;
+
+    private getValidationErrorMessage(error: string, diagram: string, uml: boolean) {
+        let message = `Please fix this mermaid parse error to make the diagram render correctly: ${error}.\n Here is the diagram you provided:\n${diagram}`;
+        if (uml) {
+            message += fixUmlMessage;
+        }
+        return message;
+    }
 }
 
 const fixUmlMessage = "\nRemember when creating the UML diagram in Mermaid, classes are represented as flat structures," +
---
+++ src/chat/mermaidPrompt.tsx
@@ -0,0 +1,148 @@
+import {
+	BasePromptElementProps,
+	PromptElement,
+	PromptSizing,
+	UserMessage
+} from '@vscode/chat-extension-utils/dist/promptTsx';
+import * as vscode from 'vscode';
+import { DiagramEditorPanel } from '../diagramEditorPanel';
+import { logMessage } from '../extension';
+import { afterIterateCommandExampleDiagram, beforeIterateCommandExampleDiagram } from './chatExamples';
+
+export interface MermaidProps extends BasePromptElementProps {
+	request: vscode.ChatRequest;
+	validationError: string | undefined;
+}
+
+export class MermaidPrompt extends PromptElement<MermaidProps, void> {
+	render(state: void, sizing: PromptSizing) {
+		const doc = vscode.window.activeTextEditor?.document;
+		// full file contents are included through the prompt references, unless the user explicitly excludes them
+		const docRef = doc ?
+			`My focus is currently on the file ${doc.uri.fsPath}` :
+			`There is not a current file open, the root of the workspace is: ${vscode.workspace.workspaceFolders?.[0]?.uri.fsPath}`;
+		const currentDiagram = DiagramEditorPanel.currentPanel?.diagram;
+		const diagramRef = currentDiagram ?
+			`Refer to this if it sounds like I'm referring to an existing diagram:\n${currentDiagram.content}` :
+			`There isn't a diagram open that you created.`;
+		const clickableSyntax = 'click {ItemLabel} call linkCallback("{ItemFilePath}#L{LineNumber}")';
+		const clickableSyntaxExample = `click A call linkCallback("myClass.ts#L42")`;
+		return (
+			<>
+				<UserMessage>
+					Instructions: <br />
+					- You are helpful chat assistant that creates diagrams using the
+					mermaid syntax. < br />
+					- If you aren't sure which tool is relevant and feel like you are missing
+					context, start by searching the code base to find general information.
+					You can call tools repeatedly to gather as much context as needed as long
+					as you call the tool with different arguments each time.Don't give up
+					unless you are sure the request cannot be fulfilled with the tools you
+					have. < br />
+					- If you find a relevant symbol in the code gather more information about
+					it with one of the symbols tools. < br />
+					- Use symbol information to find the file path and line number of the
+					symbol so that they can be referenced in the diagram. < br />
+					- The final segment of your response should always be a valid mermaid diagram
+					prefixed with a line containing  \`\`\`mermaid and suffixed with a line
+					containing \`\`\`. <br />
+					- If you have the location for an item in the diagram, make it clickable by
+					adding adding the following syntax to the end of the line: <br />
+					{clickableSyntax} <br />
+					where ItemLabel is the label in the diagram and ItemFilePath and LineNumber
+					are the location of the item, but leave off the line number if you are unsure.
+					For example: <br />
+					{clickableSyntaxExample} <br />
+					- Make sure to only use the \`/\` character as a path separator in the links.
+					<br />
+					- Do not add anything to the response past the closing \`\`\` delimiter or
+					we won't be able to parse the response correctly. <br />
+					- The \`\`\` delimiter should only occur in the two places mentioned above.
+				</UserMessage>
+				<UserMessage priority={500}>{docRef}</UserMessage>
+				<UserMessage priority={1500}>{diagramRef}</UserMessage>
+				<RequestCommand commandName={this.props.request.command ?? ''}></RequestCommand>
+				<UserMessage>{this.props.validationError}</UserMessage>
+			</>
+		);
+	}
+}
+
+interface RequestCommandProps extends BasePromptElementProps {
+	commandName: string;
+}
+
+class RequestCommand extends PromptElement<RequestCommandProps, void> {
+	render(state: void, sizing: PromptSizing) {
+		switch (this.props.commandName) {
+			case 'iterate':
+				// If diagram already exists
+				const diagram = DiagramEditorPanel.currentPanel?.diagram;
+				if (!diagram) {
+					logMessage('Iterate: No existing diagram.');
+					return (
+						<>
+							<UserMessage>
+								End this chat conversation after explaining that you cannot iterate on a diagram that does not exist.
+							</UserMessage>
+						</>
+					)
+				}
+				logMessage('Iterating on existing diagram.');
+				logMessage(diagram.content);
+				return (
+					<>
+						<UserMessage>
+							Please make changes to the currently open diagram.
+
+							There will be following instructions on how to update the diagram.
+							Do not make any other edits except my directed suggestion.
+							It is much less likely you will need to use a tool, unless the question references the codebase.
+							For example, if the instructions are 'Change all int data types to doubles and change Duck to Bunny' in the following diagram:
+							{beforeIterateCommandExampleDiagram}
+							Then you should emit the following diagram:
+							{afterIterateCommandExampleDiagram}
+						</UserMessage>
+					</>
+				)
+			case 'uml':
+				return (
+					<>
+						<UserMessage>
+							Please create UML diagram. Include all relevant classes in the file attached as context. You must use the tool mermAId_get_symbol_definition to get definitions of symbols
+							not defined in the current context. You should call it multiple times since you will likely need to get the definitions of multiple symbols.
+							Therefore for all classes you touch, explore their related classes using mermAId_get_symbol_definition to get their definitions and add them to the diagram.
+							All class relationships should be defined and correctly indicated using mermaid syntax. Also add the correct Cardinality / Multiplicity to associations like 1..n one to n where n is great than 1.
+						</UserMessage>
+						<UserMessage>
+							Remember that all class associations/should be defined! The types of relationships that you can include, and their syntax in mermaid UML diagrams, are as follows:
+							Inheritance: &lt;|-- : Represents a "is-a" relationship where a subclass inherits from a superclass.
+							Composition: *-- : Represents a "whole-part" relationship where the part cannot exist without the whole.
+							Aggregation: o-- : Represents a "whole-part" relationship where the part can exist independently of the whole.
+							Association: --&gt; : Represents a general connection between two classes.
+							Dependency: ..&gt; : Represents a "uses" relationship where one class depends on another.
+							Realization: ..|&gt; : Represents an implementation relationship between an interface and a class.
+							Link; solid or dashed: -- : used when no other relationship fits.
+						</UserMessage>
+					</>
+				);
+			case 'sequence':
+				return (
+					<UserMessage>
+						Please create a mermaid sequence diagram. The diagram should include all relevant steps to describe the behaviors, actions, and steps in the user's code.
+						Sequence diagrams model the interactions between different parts of a system in a time-sequenced manner. There are participants which represent entities in
+						the system. These actors can have aliases, be group and be deactivated.
+						Mermaid sequence diagrams also support loops, alternative routes, parallel actions, breaks in flow, notes/comments and more.
+						Use all of these features to best represent the users code and add in notes and comments to provide explanation.
+						As always, end your message with the diagram.
+					</UserMessage>
+				);
+			default:
+				return (
+					<UserMessage>
+						Pick an appropriate diagram type, for example: sequence, class, or flowchart.
+					</UserMessage>
+				);
+		}
+	}
+}
